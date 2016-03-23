1aa5d5da4696811feb39bc720451fd96db3bff5f
Ray Zane <ray@promptworks.com>
2016-03-23 17:05:28 -0400

    + Allow active record to handle join aliasing + Get rid of the whole props thing + Clone actually works fine for spawning fresh instances

--- spec/baby_squeel/table_spec.rb
+++ spec/baby_squeel/table_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 require 'baby_squeel/table'
+require 'shared_examples/table'
 
 describe BabySqueel::Table do
   subject(:table) {
     BabySqueel::Table.new(Post)
+  }
+
+  let(:association) {
+    BabySqueel::Association.new(
+      BabySqueel::Table.new(Author),
+      Author.reflect_on_association(:posts)
+    )
   }
 
   include_examples 'a table'
