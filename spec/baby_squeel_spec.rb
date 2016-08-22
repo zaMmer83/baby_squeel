f3798ab93fc5bb0e107962a3a000a6537f0d72ac
Ray Zane <ray@promptworks.com>
2016-08-22 01:12:20 -0400

    Add missing test for BabySqueel.[] with an Arel::Table

--- spec/baby_squeel_spec.rb
+++ spec/baby_squeel_spec.rb
@@ -6,6 +6,10 @@ describe BabySqueel do
       expect(BabySqueel[Post]).to be_a(BabySqueel::Relation)
     end
 
+    it 'accepts an Arel::Table' do
+      expect(BabySqueel[Post.arel_table]).to be_a(BabySqueel::Table)
+    end
+
     it 'accepts a symbol' do
       expect(BabySqueel[:posts]).to be_a(BabySqueel::Table)
     end
