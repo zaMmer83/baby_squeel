2abd8e311921069d319b72e6eb145f7eb4e5e777
Ray Zane <ray@promptworks.com>
2017-09-01 15:15:34 -0400

    Add includes, preload, and eager_load to compat

--- spec/baby_squeel/compat_spec.rb
+++ spec/baby_squeel/compat_spec.rb
@@ -23,3 +23,36 @@ describe 'BabySqueel::Compat::DSL', :compat do
     end
   end
 end
+
+describe 'BabySqueel::Compat::QueryMethods', :compat do
+  describe '#includes' do
+    it 'accepts a block' do
+      posts = Post.includes { author.posts }
+      expect(posts.includes_values).to eq([ author: { posts: {} } ])
+    end
+
+    it 'handles arrays' do
+      posts = Post.includes { [comments, author.posts] }
+      expect(posts.includes_values).to eq([ { comments: {} }, author: { posts: {} } ])
+    end
+
+    it 'handles nil' do
+      posts = Post.includes { nil }
+      expect(posts.includes_values).to eq([])
+    end
+  end
+
+  describe '#eager_load' do
+    it 'accepts a block' do
+      posts = Post.eager_load { author.posts }
+      expect(posts.eager_load_values).to eq([ author: { posts: {} } ])
+    end
+  end
+
+  describe '#preload' do
+    it 'accepts a block' do
+      posts = Post.preload { author.posts }
+      expect(posts.preload_values).to eq([ author: { posts: {} } ])
+    end
+  end
+end
