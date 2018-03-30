605d3e37dbd3116c754b5189dc60fe5870814b6a
Ray Zane <ray@promptworks.com>
2018-03-30 14:23:47 -0400

    Rename JoinExpression to Join

--- lib/baby_squeel/active_record/query_methods.rb
+++ lib/baby_squeel/active_record/query_methods.rb
@@ -39,7 +39,7 @@ module BabySqueel
       # This is a monkey patch, and I'm not happy about it.
       # Active Record will call `group_by` on the `joins`. The
       # Injector has a custom `group_by` method that handles
-      # BabySqueel::JoinExpression nodes.
+      # BabySqueel::Join nodes.
       if ::ActiveRecord::VERSION::MAJOR >= 5 && ::ActiveRecord::VERSION::MINOR >= 2
         def build_joins(manager, joins, aliases)
           super manager, BabySqueel::JoinDependency::Injector.new(joins), aliases
