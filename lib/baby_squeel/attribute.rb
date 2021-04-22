fac7aa734646b13b17cccd41abcdf914415e2115
Ray Zane <raymondzane@gmail.com>
2021-04-21 22:10:15 -0400

    Register aliases

--- lib/baby_squeel/attribute.rb
+++ lib/baby_squeel/attribute.rb
@@ -1,11 +1,24 @@
 module BabySqueel
   module Attribute
-    def ==(other)
-      eq(other)
+    def self.register(operator, arel_name)
+      define_method operator do |*args|
+        send(arel_name, *args)
+      end
     end
 
-    def !=(other)
-      neq(other)
-    end
+    register :<, :lt
+    register :>, :gt
+    register :<=, :lteq
+    register :>=, :gteq
+    register :==, :eq
+    register :'!=', :not_eq
+    register :&, :and
+    register :|, :or
+    register :=~, :matches
+    register :'!~', :does_not_match
+    register :like, :matches
+    register :not_like, :does_not_match
+    register :like_any, :matches_any
+    register :not_like_any, :does_not_match_any
   end
 end
